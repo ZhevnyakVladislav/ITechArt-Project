634cfd3e5b4a4ba2e06ae7894f8f9a7a8cef0ba0
Uladzislau Zhauniak <uladzislau.zhauniak@itechart-group.com>
2017-08-02 17:15:30 +0300

    Added roles

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Row, Col, Navbar, Button, Image } from 'react-bootstrap';
+import { Row, Col, Navbar, Nav, NavItem, Button, Image } from 'react-bootstrap';
 import './header.scss';
 
 export default class Header extends React.Component {
@@ -10,21 +10,47 @@ export default class Header extends React.Component {
                 <Button onClick={this.props.logOut}>Log Out</Button>
             ) : (<Button> Log In</Button>)
         );
+
+        const header = (
+            (this.props.isUserAuth) ? (
+                <Col xsHidden sm={9}>
+                    <Col sm={7} className='links'>
+                        <Nav>
+                            <NavItem eventKey={1} href="/newadvert">new advert</NavItem>
+                            <NavItem eventKey={2} href="/alladvert">all advert</NavItem>
+                        </Nav>
+                    </Col>
+                    <Col sm={3} className='avatar'>
+                        <LinkContainer to="/login">
+                            <div>
+                                <h4>Vladislav</h4>
+                                <Image src="http://via.placeholder.com/50x50" circle />
+                            </div>
+                        </LinkContainer>
+                    </Col>
+                    <Col sm={2}>
+                        <LinkContainer to="/login">
+                            {button}
+                        </LinkContainer>
+                    </Col>
+                </Col>
+            ) : (
+                <Col xsHidden smOffset={7} sm={3}>
+                    {button}
+                </Col>
+            )
+        );
         return (
             <header>
                 <Navbar fluid>
-                    <Navbar.Toggle onClick={this.props.openDrawer}/>
                     <Row> 
-                        <Col xs={8}>
+                        <Col xs={10} sm={3}>
                             <LinkContainer to="/">
                                 <Image  src='https://www.itechart.com/static/img/logo.png'/>
                             </LinkContainer>
                         </Col>
-                        <Col xs={3} xsHidden={true}>
-                            <LinkContainer to="/login">
-                                {button}
-                            </LinkContainer>
-                        </Col>
+                        {header}
+                        <Navbar.Toggle onClick={this.props.openDrawer}/>
                     </Row>
                 </Navbar>
             </header>
--- client/components/LeftDrawer/LeftDrawer.js
+++ client/components/LeftDrawer/LeftDrawer.js
@@ -11,41 +11,51 @@ export default class LeftDrawer extends React.Component {
                 <LinkContainer to='/account' className="menu-item">
                     <Col xs={12} className='avatar'>
                         <Image src="http://via.placeholder.com/70x70" circle />
-                        <h3>NickName</h3>
+                        <h3>Vladislav Zgevnyak</h3>
                     </Col>
                 </LinkContainer>
             ) : (
                 <LinkContainer to='/login' className="menu-item">
                     <Col xs={12} className='avatar'>
                         <Image src="http://via.placeholder.com/70x70" circle />
-                        <h3>NickName</h3>
+                        <h3>Please, login</h3>
                     </Col>
                 </LinkContainer>
             )
         );
+
+        const links = (
+            (this.props.isUserAuth) ? (
+                <div>
+                    <LinkContainer to='/newadvert' className="menu-item">
+                        <Col xs={12} >
+                            <h4>Create new advert</h4>
+                        </Col>
+                    </LinkContainer>
+                    <LinkContainer to='/alladverts' className="menu-item">
+                        <Col xs={12} >
+                            <h4>All adverts</h4>
+                        </Col>
+                    </LinkContainer>
+                </div>
+            ) : (null)
+        );
+
+        const logOut = (this.props.isUserAuth ? (
+            <LinkContainer to="/login" className='logout'>
+                <Col xs={12}>
+                    <Button onClick={this.props.logOut}>Log Out</Button>
+                </Col>
+            </LinkContainer>
+        ):(null));
+
         return (
             <Grid id='drawer-overlay' className={this.props.isDrawerOpen ? 'opened' : 'closed'} onClick={this.props.closeDrawer}>
                 <Row>
                     <Col xs={8} id='drawer' className={this.props.isDrawerOpen ? 'opened' : 'closed'}>
                         {avatar}
-                        <LinkContainer to='/newadvert' className="menu-item">
-                            <Col xs={12} >
-                                <h4>Create new advert</h4>
-                            </Col>
-                        </LinkContainer>
-                        <LinkContainer to='/alladverts' className="menu-item">
-                            <Col xs={12} >
-                                <h4>all adverts</h4>
-                            </Col>
-                        </LinkContainer>
-                        {this.props.isUserAuth ? (
-                            <LinkContainer to="/login" className='logout'>
-                                <Col xs={12}>
-                                    <Button onClick={this.props.logOut}>Log Out</Button>
-                                </Col>
-                            </LinkContainer>
-                        ):(null)}
-                        
+                        {links}
+                        {logOut}
                     </Col>
                 </Row>
             </Grid>
